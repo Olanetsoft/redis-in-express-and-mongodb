b4d8ddad864b2a225c6f91af4308f1d44ae04cf0
olanetsoft <heedris2olubisi@gmail.com>
2020-08-25 15:27:36 +0100

    routes implemented

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
     MONGO_URI: 'mongodb+srv://idris:Hayindehdb2019@cluster0-sszay.mongodb.net/articlesDB?retryWrites=true&w=majority',
-    REDIS_URI: 'YOUR_REDIS_CONNECTION_STRING_GOES_HERE'
+    REDIS_URI: '127.0.0.1:6379'
 };
